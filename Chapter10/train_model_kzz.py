423e847e5274678beafc2d8d858eb8515c7f6b87
unknown <qinlin.liu@gticom.cn>
2020-08-22 00:23:09 +0800

    kzz_data

--- Chapter10/train_model_kzz.py
+++ Chapter10/train_model_kzz.py
@@ -13,12 +13,14 @@ from ignite.contrib.handlers import tensorboard_logger as tb_logger
 
 from lib import environ, data, models, common, validation
 import os
-save_dir = 'drive/My Drive/h_gym/saves/'
+
+save_dir = '/content/drive/My Drive/h_gym/saves/'
 try:
     os.makedirs(save_dir)
 except:
     pass
 SAVES_DIR = pathlib.Path(save_dir)
+print(SAVES_DIR)
 STOCKS = "data/kzz_SH110044.csv"
 VAL_STOCKS = "data/kzz_SH113017.csv"
 
@@ -37,7 +39,6 @@ REWARD_STEPS = 2
 LEARNING_RATE = 0.0001
 STATES_TO_EVALUATE = 1000
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", help="Enable cuda", default=False, action="store_true")
@@ -49,6 +50,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if args.cuda else "cpu")
 
     saves_path = SAVES_DIR / f"simple-{args.run}"
+    print(saves_path)
     saves_path.mkdir(parents=True, exist_ok=True)
 
     data_path = pathlib.Path(args.data)
@@ -89,6 +91,7 @@ if __name__ == "__main__":
         exp_source, REPLAY_SIZE)
     optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
 
+
     def process_batch(engine, batch):
         optimizer.zero_grad()
         loss_v = common.calc_loss(
@@ -109,9 +112,11 @@ if __name__ == "__main__":
             "epsilon": selector.epsilon,
         }
 
+
     engine = Engine(process_batch)
     tb = common.setup_ignite(engine, exp_source, f"simple-{args.run}",
                              extra_metrics=('values_mean',))
+
 
     @engine.on(ptan.ignite.PeriodEvents.ITERS_1000_COMPLETED)
     def sync_eval(engine: Engine):
@@ -129,6 +134,7 @@ if __name__ == "__main__":
             path = saves_path / ("mean_value-%.3f.data" % mean_val)
             torch.save(net.state_dict(), path)
             engine.state.best_mean_val = mean_val
+
 
     @engine.on(ptan.ignite.PeriodEvents.ITERS_10000_COMPLETED)
     def validate(engine: Engine):
@@ -150,6 +156,7 @@ if __name__ == "__main__":
             engine.state.best_val_reward = val_reward
             path = saves_path / ("val_reward-%.3f.data" % val_reward)
             torch.save(net.state_dict(), path)
+
 
     event = ptan.ignite.PeriodEvents.ITERS_10000_COMPLETED
     tst_metrics = [m + "_tst" for m in validation.METRICS]
