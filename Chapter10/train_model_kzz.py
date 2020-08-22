d6f9203f9e88adb9c9ff5640668e007b872b71fe
unknown <qinlin.liu@gticom.cn>
2020-08-22 16:55:45 +0800

    kzz_data

--- Chapter10/train_model_kzz.py
+++ Chapter10/train_model_kzz.py
@@ -41,12 +41,13 @@ STATES_TO_EVALUATE = 1000
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cuda", help="Enable cuda", default=True, action="store_true")
+    parser.add_argument("--cuda", help="Enable cuda", default=False, action="store_true")
     parser.add_argument("--data", default=STOCKS, help=f"Stocks file or dir, default={STOCKS}")
     parser.add_argument("--year", type=int, help="Year to train on, overrides --data")
     parser.add_argument("--val", default=VAL_STOCKS, help="Validation data, default=" + VAL_STOCKS)
     parser.add_argument("-r", "--run", required=True, help="Run name")
     args = parser.parse_args()
+    print(args)
     device = torch.device("cuda" if args.cuda else "cpu")
 
     saves_path = SAVES_DIR / f"simple-{args.run}"
