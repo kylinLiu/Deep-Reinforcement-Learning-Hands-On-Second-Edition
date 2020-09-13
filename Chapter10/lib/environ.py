e034417572e2195f0b77a363d20b6e011e6b6218
unknown <qinlin.liu@gticom.cn>
2020-09-13 17:10:03 +0800

    Chapter10/lib/environ.py

--- Chapter10/lib/environ.py
+++ Chapter10/lib/environ.py
@@ -118,6 +118,75 @@ class State:
 
         return reward, done
 
+    def step2(self, action):
+        """
+        Perform one step in our price, adjust offset, check for the end of prices
+        and handle position change
+        :param action:
+        :return: reward, done
+        """
+        assert isinstance(action, Actions)
+        reward = 0.0
+        done = False
+        real_closes = self._prices.real_close.tolist()
+        len_all = len(real_closes)
+        after_offset = self._offset + 60
+        after_offset = len_all if after_offset > len_all else after_offset
+        pre_offset = 0 if after_offset < 120 else after_offset - 120
+        after_offset = pre_offset + 120
+
+        close = real_closes[self._offset]
+        pre_closes = real_closes[pre_offset:self._offset+1]
+        max_pre_close = max(pre_closes)
+        max_pre_index = pre_closes.index(max_pre_close)
+        print("max_pre_index", max_pre_index)
+        print("len(pre_closes)", len(pre_closes))
+        min_pre_close = close
+        if pre_closes[max_pre_index:]:
+            min_pre_close = min(pre_closes[max_pre_index:])
+        pre_reward = ((max_pre_close - close) / close) - ((close - min_pre_close) / min_pre_close)
+
+        after_closes = real_closes[self._offset:after_offset+1]
+
+        max_after_close = max(after_closes)
+
+        max_after_index = after_closes.index(max_after_close)
+        print("max_after_index", max_after_index)
+        print("len(after_closes)", len(after_closes))
+        min_after_close = close
+        if after_closes[max_after_index:]:
+            min_after_close = min(pre_closes[min_after_close:])
+        # min_after_close = min(after_closes[:max_after_index])
+
+        after_reward = ((max_after_close - close) / close) - ((close - min_after_close) / min_after_close)
+        org_reward = after_reward - pre_reward
+
+        # after_closes = self._prices.real_close[self._offset:]
+        #
+        # after_max_close = max(pre_closes)
+        # after_min_close = min(pre_closes)
+        #
+        # close = self._cur_close()
+        if action == Actions.Buy:
+            reward = org_reward
+        elif action == Actions.Close:
+            reward = -1 * org_reward
+        elif action == Actions.Skip and self.have_position:
+            reward = org_reward
+        elif action == Actions.Skip and not self.have_position:
+            reward = -1 * org_reward
+
+        if action == Actions.Buy and not self.have_position:
+            self.have_position = True
+            self.open_price = close
+        elif action == Actions.Close and self.have_position:
+            done |= self.reset_on_close
+            self.have_position = False
+
+        self._offset += 1
+        done |= self._offset >= self._prices.close.shape[0] - 1
+
+        return reward, done
     def step(self, action):
         """
         Perform one step in our price, adjust offset, check for the end of prices
@@ -136,37 +205,19 @@ class State:
         after_offset = pre_offset + 120
 
         close = real_closes[self._offset]
-        pre_closes = real_closes[pre_offset:self._offset]
+        pre_closes = real_closes[pre_offset:self._offset+1]
         max_pre_close = max(pre_closes)
-        max_pre_index = pre_closes.index(max_pre_close)
-        print("max_pre_index", max_pre_index)
-        print("len(pre_closes)", len(pre_closes))
-        min_pre_close = close
-        if pre_closes[max_pre_index:]:
-            min_pre_close = min(pre_closes[max_pre_index:])
+        min_pre_close = min(pre_closes)
         pre_reward = ((max_pre_close - close) / close) - ((close - min_pre_close) / min_pre_close)
 
-        after_closes = real_closes[self._offset:after_offset]
+        after_closes = real_closes[self._offset:after_offset+1]
 
         max_after_close = max(after_closes)
-
-        print("max_after_index", max_after_index)
-        print("len(after_closes)", len(after_closes))
-        max_after_index = after_closes.index(max_after_close)
-        min_after_close = close
-        if after_closes[max_after_index:]:
-            min_after_close = min(pre_closes[min_after_close:])
-        # min_after_close = min(after_closes[:max_after_index])
+        min_after_close = min(after_closes)
 
         after_reward = ((max_after_close - close) / close) - ((close - min_after_close) / min_after_close)
         org_reward = after_reward - pre_reward
 
-        # after_closes = self._prices.real_close[self._offset:]
-        #
-        # after_max_close = max(pre_closes)
-        # after_min_close = min(pre_closes)
-        #
-        # close = self._cur_close()
         if action == Actions.Buy:
             reward = org_reward
         elif action == Actions.Close:
